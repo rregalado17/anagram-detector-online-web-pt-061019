c6d8a396568d488bed6dd893beeaf4777934f1b0
Robinson Regalado <rregalado17@gmail.com>
2019-07-18 20:19:46 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,5 +7,11 @@ class Anagram
     @word_entered = word_entered
   end 
 
-  
+  def match(find_anagram)
+    find_anagram.final_all do |word|
+      if word.split("").sort == word_entered.split("").sort
+        word_entered
+  end
+end 
+
 end
