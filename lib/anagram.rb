cdaaaa910981c9f4ee3838388a58e4b83c90d12d
Robinson Regalado <rregalado17@gmail.com>
2019-07-18 20:12:43 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,5 +1,19 @@
 # Your code goes here!
 
 class Anagram
-  attr_accessor :word 
+  attr_accessor :word_entered 
+  
+  def initialize(word_entered)
+    @word_entered = word_entered
+  end 
+  
+  def match(find_anagram)
+    find_anagram.find_all do |word| 
+      if word.split("").sort == self.word_entered("").sort
+        
+      end
+    end 
+  end
+  
+  
 end
